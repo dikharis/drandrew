bcd658521c4c3581e2193be7b5b9857c79c95d83
dikharis <90677929+dikharis@users.noreply.github.com>
2026-01-03 21:35:34 +0800

    feat: Update project configuration and dependencies
    Refactors project setup to use Vite defaults and updates dependencies.
    Removes custom Vite configuration, simplifies `tsconfig.json`, and updates `package.json` with current versions. Also, removes Vercel-specific configuration as it's no longer needed.

---
+++ vite-env.d.ts
@@ -0,0 +1,43 @@
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+interface ImportMetaEnv {
+  [key: string]: any
+  BASE_URL: string
+  MODE: string
+  DEV: boolean
+  PROD: boolean
+  SSR: boolean
+}
+
+interface ImportMeta {
+  url: string
+  readonly env: ImportMetaEnv
+}
